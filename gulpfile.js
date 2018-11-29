0051cebdb8be82ae644093c2f157a1754b8608aa
Ricardo E. Galvez <galvez667@gmail.com>
2018-11-29 14:41:43 -0500

    :meat_on_bone:  after much trial and error, got font loading to work properly, but in that process blew up the gulp works... so that needs a big refactor i think, but need to move out of this branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-'use strict';
 const gulp = require('gulp');
 
 const sass = require('gulp-sass');
@@ -11,136 +10,129 @@ const concat = require('gulp-concat');
 
 const htmlmin = require('gulp-htmlmin');
 
-//const browserSync = require('browser-sync').create();
-
 const del = require('del');
 
 const critical = require('critical').stream;
 
-// variables used in our sass tasks
-const cssInput = 'src/sass/**/*.scss';
+const log = require('fancy-log');
+//
+// main gulp tasks
+//
+
+// SASS Tasks
+// Path from  SASS files scss input -> compiled css output path
+const scssInput = 'src/sass/**/*.scss';
 const cssOutput = 'dist/css';
 
-// options for (S)CSS
-const sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'compressed'
-};
-
-const jsInput = 'src/js/**/*.js';
-const jsOutput = 'dist/js';
-// JS Sources in order:
-const jsSources = {
-  sources: [
-    './src/js/vendor/modernizr-custom.js', 
-    './src/js/plugins.js', 
-    './src/js/main.js'
-  ]
-}
-
-const criticalHTML = {
-  in: './src/html/**/*.html',
-  out: './src/chtml'
-};
-
-const htmlInput = './src/chtml/**/*.*html';
-const htmlOutput = 'dist/';
-
-
-// autoprefixer options
+// CSS autoprefixer options
 // this covers 90.87% of all browsers. run npx autoprefixer --info to see full report.
 const autoprefixerOptions = {
   browsers: [
     'last 3 versions',
-    '> 5%', 'Firefox ESR'
+    '> 5%', 'Firefox ESR',
   ],
   flexbox: 'true',
-  grid: 'true'
+  grid: 'true',
 };
 
-//
-// main gulp tasks
-//
-gulp.task('sass', () => {
-  return gulp.src(cssInput)
-    .pipe(sass())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(cssOutput));
-    //.pipe(browserSync.reload({ stream : true }));
-});
+// SASS options
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'compressed',
+};
 
-gulp.task('js', () => {
-  return gulp.src(jsSources.sources)
-    .pipe(sourcemaps.init())
-    .pipe(concat('main.min.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(jsOutput));
-    
-});
+// set up the SASS task:
+gulp.task('sass', () => gulp.src(scssInput)
+  .pipe(sass())
+  .pipe(autoprefixer(autoprefixerOptions))
+  .pipe(sourcemaps.init())
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest(cssOutput)));
 
+// end SASS works
+
+//
+// Find and concact all the JS into a single file:
+//
+
+// JS Sources in _must_ be in order of execution
+const jsOutput = 'dist/js';
+const jsSources = {
+  sources: [
+    './src/js/vendor/modernizr-custom.js',
+    './src/js/plugins.js',
+    './src/js/main.js',
+  ],
+};
+gulp.task('js', () => gulp.src(jsSources.sources)
+  .pipe(sourcemaps.init())
+  .pipe(concat('main.min.js'))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest(jsOutput)));
+
+
+//
 // Generate & Inline Critical-path CSS
+// paths for critical operations:
+const criticalHTML = {
+  in: 'src/html/**/*.html',
+  out: 'src/chtml',
+};
+
 const criticalOptions = {
-  base: criticalHTML.out,
+  base: 'src/',
   inline: true,
   css: 'dist/css/main.css',
-  }
+};
 
 gulp.task('critical', () => {
-  return gulp.src(criticalHTML.in)
-      .pipe(critical(criticalOptions))
-      .on('error', function(err) { log.error(err.message); })
-      .pipe(gulp.dest(criticalHTML.out));
+  gulp.src(criticalHTML.in)
+    .pipe(critical(criticalOptions))
+    .on('error', (err) => { log.error(err.message); })
+    .pipe(gulp.dest(criticalHTML.out));
 });
 
+// end critical task
+
+//
 // Minify the whole thing _after_ Critical has done its work...
-// options for HTML Minification
-const htmlOptions = { 
-  collapseWhitespace: true
-}
+// paths for HTML Minification
+const chtmlInput = 'src/chtml/**/*.*html';
+const htmlOutput = 'dist/';
 
-gulp.task('minify-html', () => {
-  return gulp.src(htmlInput)
-    .pipe(htmlmin(htmlOptions))
-    .pipe(gulp.dest(htmlOutput));
-});
+// minify html options:
+const htmlOptions = {
+  collapseWhitespace: true,
+};
+
+gulp.task('minify-html', () => gulp.src(chtmlInput)
+  .pipe(htmlmin(htmlOptions))
+  .pipe(gulp.dest(htmlOutput)));
+
+// end minifications
 
 // run clean up operations:
 // delete current occupants of the chtml (basically the "temp" folder. stands for "criticized HTML")
 
-// .pipe(del(['./src/chtml/*'], { read: false }))
+const cleanDest = {
+  sources: [
+    'src/chtml/**/*',
+    'dist/index.html',
+    'dist/404.html',
+    'dist/css',
+    'dist/js/*.js',
+    'dist/about',
+    '!dist/js/vendor',
+  ],
+};
 
-gulp.task('clean', () => {
-  // return del(criticalHTML.out);
-  return del(['./src/chtml/*'], { read: false });
-});
+gulp.task('clean', () => del(cleanDest.sources, { read: false }));
 
-// gulp.task('serve', function() {
-//   browserSync.init({
-//     server: {
-//       baseDir: './'
-//     },
-//     //port : 5678
-//     //proxy : 'http://192.168.1.155:5678'
-//   });
-
-//   browserSync.init({
-//    // port : 5678
-//    proxy: 'localhost:5678'
-// });
-
-//   gulp.watch('sass/**/*.scss', ['sass']);
-//   gulp.watch('./**/*.html').on('change', browserSync.reload);
-// });
-
-// 
-// Critical CSS inlining:
-//
+// Set up a watch command:
 // TODO: this watch command DOES not work just yet.
-//       so it should be fixed...
-// 
+//
+//
 // gulp.task('watch', () => {
 //   gulp.watch(criticalHTML.in, ['critical']);
 //   gulp.watch(htmlInput, ['minify-html']);
@@ -149,5 +141,6 @@ gulp.task('clean', () => {
 // });
 
 gulp.task('clean-it', ['clean']);
+gulp.task('crit', ['critical']);
 
-gulp.task('default', ['critical', 'sass', 'js', 'minify-html']);
+gulp.task('default', ['sass', 'js', 'critical', 'minify-html']);
